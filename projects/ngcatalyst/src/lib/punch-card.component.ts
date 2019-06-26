92d1ef0df2705cc5dbf507d689e25ab593059785
Laura Shapiro <laurashapiro01@gmail.com>
2019-06-26 15:33:13 -0400

    different method for change detection

--- projects/ngcatalyst/src/lib/punch-card.component.ts
+++ projects/ngcatalyst/src/lib/punch-card.component.ts
@@ -28,15 +28,15 @@ export class PunchCardComponent implements OnInit, OnChanges, AfterViewInit {
   // @Input() divWidth = 750;
   // tslint:disable-next-line:max-line-length
   labelsX = ["12a", "1a", "2a", "3a", "4a", "5a", "6a", "7a", "8a", "9a", "10a", "11a", "12p", "1p", "2p", "3p", "4p", "5p", "6p", "7p", "8p", "9p", "10p", "11p"];
-  
+
   constructor() { }
-  
+
   // get area () {
   //   let height = this.divHeight + "px";
   //   let width = this.divWidth + "px";
   //   return {height: height, width: width}
   // }
-  
+
   ngOnInit() {
     this.drawPunchCard();
   }
